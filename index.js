c5e5f1978412e56f1ef6ca8feebfde2aa34e14be
mdayann <mdayan312@gmail.com>
2021-01-25 00:31:45 +0700

    Set up boilerplate

---
+++ index.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+server.listen(PORT, () => console.log(`🚀 Server has started on port ${PORT}`));
