8ff6d725e6ffe846e943639cd24437aef67e8174
mdayann <mdayan312@gmail.com>
2021-02-11 13:01:12 +0700

    modify port

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, { origins: '*:*' });
 
 app.use(cors());
 app.use(router);
