c7bec6fa5db34bbab100da2565bc3101243b7171
mdayann <mdayan312@gmail.com>
2021-02-15 16:56:46 +0700

    refactor socketio

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
-
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server, { origins: '*:*' });
@@ -19,55 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Parse requests of content-type - application/json
 app.use(bodyParser.json());
 
-io.on('connection', (socket) => {
-  // eslint-disable-next-line consistent-return
-  socket.on('join', ({ name, room }, callback) => {
-    const { error, user } = addUser({ id: socket.id, name, room });
-
-    if (error) return callback(error);
-
-    socket.join(user.room);
-
-    socket.emit('message', {
-      user: 'admin',
-      text: `${user.name}, welcome to room ${user.room}.`,
-    });
-    socket.broadcast
-      .to(user.room)
-      .emit('message', { user: 'admin', text: `${user.name} has joined!` });
-
-    io.to(user.room).emit('roomData', {
-      room: user.room,
-      users: getUsersInRoom(user.room),
-    });
-
-    callback();
-  });
-
-  socket.on('sendMessage', (message, callback) => {
-    const user = getUser(socket.id);
-
-    io.to(user.room).emit('message', { user: user.name, text: message });
-    io.to(user.room).emit('roomData', {
-      room: user.room,
-      users: getUsersInRoom(user.room),
-    });
-
-    callback();
-  });
-
-  socket.on('disconnect', () => {
-    const user = removeUser(socket.id);
-
-    if (user) {
-      io.to(user.room).emit('message', {
-        user: 'admin',
-        text: `${user.name} has left`,
-      });
-    }
-  });
-});
-
+require('./app/controllers/socketio/socketio')(io);
 require('./app/routes/routes')(app);
 
 server.listen(5000, () => console.log(`🚀 Server has started on port 5000`));
